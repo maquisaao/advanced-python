49fddfa6b7ceb597f2e9078081e8198aac2ab01e
maquisaao <maxwpinheiro@gmail.com>
2025-09-01 22:31:30 -0300

    conversao de tipos

--- logica_basica.py
+++ logica_basica.py
@@ -72,3 +72,13 @@ print(type(True))
 print(type(False))
 print(type(10 == 10))
 print(type(10 == 11))
+
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+#  tipos imutáveis e primitivos:
+# str, int, float, bool
+print(int('1'), type(int('1')))
+print(type(float('1') + 1))
+print(bool(' '))
+print(str(11) + 'b')
