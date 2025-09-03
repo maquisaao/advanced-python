b4f79c8612b2259c47a7ebc14f30367c0859c1db
maquisaao <maxwpinheiro@gmail.com>
2025-09-03 15:17:15 -0300

    exercicio1 feito

---
+++ exercicio-imc.py
@@ -0,0 +1,11 @@
+'''
+mostrar na tela:
+Max Wilson tem 1.77 de altura, pesa 80 quilos e seu IMC é 25.4
+'''
+
+nome = "Max Wilson"
+altura = 1.77
+peso = 80
+imc = peso / (altura * altura)
+
+print(nome, 'tem', altura, 'de altura, pesa', peso, 'kgs e seu IMC é', imc)
