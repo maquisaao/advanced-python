8f31100e1441efb36e21f240758ea101b5aeb93d
maquisaao <maxwpinheiro@gmail.com>
2025-09-07 19:15:12 -0300

    correcao de textos

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -61,9 +61,9 @@ print(not False)  # True
 
 # Operadores in e not in
 # Strings são iteráveis
-#  0 1 2 3 4 5 6 7
+#  0 1 2 3 4 5 6 7 indices positivos
 #  P i n h e i r o
-#  8-7-6-5-4-3-2-1
+# -8-7-6-5-4-3-2-1 indices negativos
 
 nome = 'Pinheiro'
 print(nome[2])
