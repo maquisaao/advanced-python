202e7db353bbc0d1b87c37bdefa7ec1b23c423a4
maquisaao <maxwpinheiro@gmail.com>
2025-09-07 19:09:42 -0300

    operadores in e not in

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -57,3 +57,27 @@ not False = True
 # senha = input('Senha: ')
 print(not True)  # False
 print(not False)  # True
+
+
+# Operadores in e not in
+# Strings são iteráveis
+#  0 1 2 3 4 5 6 7
+#  P i n h e i r o
+#  8-7-6-5-4-3-2-1
+
+nome = 'Pinheiro'
+print(nome[2])
+print(nome[-4])
+print('iro' in nome)
+print('zero' in nome)
+print(10 * '-')
+print('iro' not in nome)
+print('zero' not in nome)
+
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+    print(f'{encontrar} não está em {nome}')
