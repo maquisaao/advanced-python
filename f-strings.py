eb9da6cd7e34b5b3f802f46ea0730dc85990ee5b
maquisaao <maxwpinheiro@gmail.com>
2025-09-09 20:53:18 -0300

    complementacao de f string

--- f-strings.py
+++ f-strings.py
@@ -15,3 +15,30 @@ linha_3 = f'{imc:.2f}'
 print(linha_1)
 print(linha_2)
 print(linha_3)
+
+#----------------------- parte 2 -----------------------
+
+"""
+Formatação básica de strings
+s - string
+d - int
+f - float
+.<número de dígitos>f
+x ou X - Hexadecimal
+(Caractere)(><^)(quantidade)
+> - Esquerda
+< - Direita
+^ - Centro
+= - Força o número a aparecer antes dos zeros
+Sinal - + ou -
+Ex.: 0>-100,.1f
+Conversion flags - !r !s !a 
+"""
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel: ^10}.')
+print(f'{1000.4873648123746:0=+10,.1f}')
+print(f'O hexadecimal de 1500 é {1500:08X}')
+print(f'{variavel!r}')
