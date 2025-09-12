a2da0af994d7dd54b43e1a4231118dc2232c83a5
maquisaao <maxwpinheiro@gmail.com>
2025-09-12 20:04:57 -0300

    correcao do exercicio

--- exercicio_strings.py
+++ exercicio_strings.py
@@ -21,11 +21,12 @@ if nome and idade:
     print(f'seu nome é {nome}')
     print(f'seu nome invertido é {nome[::-1]}')
     if ' ' in nome:
-        print('seu nome contém espaços')
+        print(f'seu nome contém espaços')
     else:
-        print('seu nome não contém espaços')
-    print(f'seu nome tem {len(nome)} letras')
-    print(f'a primeira letra do seu nome é {nome[0]}')  
-    print(f'a última letra do seu nome é {nome[-1]}')
+        print(f'seu nome não contem espaços')
+    print(f'seu nome contem', len(nome), 'letras')
+    print(f'a primeira letra do seu nome é', nome[0])
+    print(f'a ultima letra do seu nome é', nome[-1]) 
 else:
-    print('Desculpe, você deixou campos vazios.')
+    print(f'Desculpe, você deixou campos vazios.')
+    
