7e249557e46884582aac26411e6fedfdd669c8c5
maquisaao <maxwpinheiro@gmail.com>
2025-09-17 22:30:08 -0300

    minha resolucao exercicio 2

--- exercicio_aula_54.py
+++ exercicio_aula_54.py
@@ -5,49 +5,65 @@ inteiro, informe que não é um número inteiro.
 """
 # ------------minha solução-----------------------------
 
-valor = input('Digite um numero inteiro:')
+# valor = input('Digite um numero inteiro:')
 
-if valor.isdigit():
-    if int(valor) % 2 == 0:
-        print('Esse valor é par')
-    else:
-        print('Esse valor é impar')
-else:
-    print('Voce nao digitou um valor inteiro')
+# if valor.isdigit():
+#     if int(valor) % 2 == 0:
+#         print('Esse valor é par')
+#     else:
+#         print('Esse valor é impar')
+# else:
+#     print('Voce nao digitou um valor inteiro')
 
 # -------------solução 1 do professor----------------------------
 
-entrada = input('Digite um número: ')
+# entrada = input('Digite um número: ')
 
-if entrada.isdigit():
-    entrada_int = int(entrada)
-    par_impar = entrada_int % 2 == 0
-    par_impar_texto = 'ímpar'
+# if entrada.isdigit():
+#     entrada_int = int(entrada)
+#     par_impar = entrada_int % 2 == 0
+#     par_impar_texto = 'ímpar'
 
-    if par_impar:
-        par_impar_texto = 'par'
+#     if par_impar:
+#         par_impar_texto = 'par'
 
-    print(f'O número {entrada_int} é {par_impar_texto}')
-else:
-    print('Você não digitou um número inteiro')
+#     print(f'O número {entrada_int} é {par_impar_texto}')
+# else:
+#     print('Você não digitou um número inteiro')
+
 # --------------solução com try except---------------------------
-try:
-    entrada_int = float(entrada)
-    par_impar = entrada_int % 2 == 0
-    par_impar_texto = 'ímpar'
+# try:
+#     entrada_int = float(entrada)
+#     par_impar = entrada_int % 2 == 0
+#     par_impar_texto = 'ímpar'
 
-    if par_impar:
-        par_impar_texto = 'par'
+#     if par_impar:
+#         par_impar_texto = 'par'
 
-    print(f'O número {entrada_int} é {par_impar_texto}')
-except:
-    print('Você não digitou um número inteiro')
+#     print(f'O número {entrada_int} é {par_impar_texto}')
+# except:
+#     print('Você não digitou um número inteiro')
 
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
 descrito, exiba a saudação apropriada. Ex. 
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
+# ------------minha solução-----------------------------
+
+hora = input(f'Digite a hora: ')
+hora_int = int(hora)
+
+if hora_int <= 11:
+    print(f'Bom dia')
+
+elif hora_int > 11 and hora_int <= 17:
+    print(f'Boa tarde')
+
+else:
+    print(f'Boa noite')
+
+
 
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
