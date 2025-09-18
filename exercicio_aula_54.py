e650f0ea2cef746518ff930f5910375d1c26c4f6
maquisaao <maxwpinheiro@gmail.com>
2025-09-17 21:36:17 -0300

    minha resolução do exercicio 1

--- exercicio_aula_54.py
+++ exercicio_aula_54.py
@@ -3,31 +3,15 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
-entrada = input('Digite um número: ')
+valor = input('Digite um numero inteiro:')
 
-# if entrada.isdigit():
-#     entrada_int = int(entrada)
-#     par_impar = entrada_int % 2 == 0
-#     par_impar_texto = 'ímpar'
-
-#     if par_impar:
-#         par_impar_texto = 'par'
-
-#     print(f'O número {entrada_int} é {par_impar_texto}')
-# else:
-#     print('Você não digitou um número inteiro')
-
-try:
-    entrada_int = float(entrada)
-    par_impar = entrada_int % 2 == 0
-    par_impar_texto = 'ímpar'
-
-    if par_impar:
-        par_impar_texto = 'par'
-
-    print(f'O número {entrada_int} é {par_impar_texto}')
-except:
-    print('Você não digitou um número inteiro')
+if valor.isdigit():
+    if int(valor) % 2 == 0:
+        print('Esse valor é par')
+    else:
+        print('Esse valor é impar')
+else:
+    print('Voce nao digitou um valor inteiro')
 
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
