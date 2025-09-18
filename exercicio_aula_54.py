d77440902faa520327ebd07b8bf14f88a782186b
maquisaao <maxwpinheiro@gmail.com>
2025-09-17 21:40:04 -0300

    solução 1 do professor

--- exercicio_aula_54.py
+++ exercicio_aula_54.py
@@ -13,6 +13,22 @@ if valor.isdigit():
 else:
     print('Voce nao digitou um valor inteiro')
 
+entrada = input('Digite um número: ')
+
+if entrada.isdigit():
+    entrada_int = int(entrada)
+    par_impar = entrada_int % 2 == 0
+    par_impar_texto = 'ímpar'
+
+    if par_impar:
+        par_impar_texto = 'par'
+
+    print(f'O número {entrada_int} é {par_impar_texto}')
+else:
+    print('Você não digitou um número inteiro')
+
+    
+
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
 descrito, exiba a saudação apropriada. Ex. 
