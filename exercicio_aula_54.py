c7e74180b3509f8c878f1fbea142f0bb6bbf9eeb
maquisaao <maxwpinheiro@gmail.com>
2025-09-17 22:30:27 -0300

    resolucao do professor exercicio 2

--- exercicio_aula_54.py
+++ exercicio_aula_54.py
@@ -63,6 +63,23 @@ elif hora_int > 11 and hora_int <= 17:
 else:
     print(f'Boa noite')
 
+# -------------solução 1 do professor----------------------------
+
+entrada = input('Digite a hora em números inteiros: ')
+
+try:
+    hora = int(entrada)
+
+    if hora >= 0 and hora <= 11:
+        print('Bom dia')
+    elif hora >= 12 and hora <= 17:
+        print('Bom tarde')
+    elif hora >= 18 and hora <= 23:
+        print('Bom noite')
+    else:
+        print('Não conheço essa hora')
+except:
+    print('Por favor, digite apenas números inteiros')
 
 
 """
