2587bf9c69869d6c09ac704a794247b4f0e6f959
maquisaao <maxwpinheiro@gmail.com>
2025-09-20 19:33:13 -0300

    introducao ao laco while

---
+++ while.py
@@ -0,0 +1,22 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+condicao = True
+
+while condicao:
+    nome = input('Qual o seu nome: ')
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break
+
+print('Acabou')
+
+contador = 0 
+
+while contador < 10:
+    print(f'{contador}')
+    contador += 1
