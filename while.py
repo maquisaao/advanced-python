11865ad48275ea42e437349c805653f9901c1a11
maquisaao <maxwpinheiro@gmail.com>
2025-09-20 19:42:54 -0300

    usando continue para pular acoes

--- while.py
+++ while.py
@@ -11,7 +11,7 @@ while condicao:
     print(f'Seu nome é {nome}')
 
     if nome == 'sair':
-        break
+        break # sai do bloco
 
 print('Acabou')
 
@@ -19,4 +19,30 @@ contador = 0
 
 while contador < 10:
     print(f'{contador}')
+    contador += 1 # mesmo que ( contador = contador + 1 )
+
+# = += -= *= /= //= **= %=
+
+'''
+usando continue para 'pular' acoes
+'''
+contador = 0
+
+while contador <= 100:
     contador += 1
+
+    if contador == 6:
+        print('Não vou mostrar o 6.')
+        continue
+
+    if contador >= 10 and contador <= 27:
+        print('Não vou mostrar o', contador)
+        continue
+
+    print(contador)
+
+    if contador == 40:
+        break
+
+
+print('Acabou')
