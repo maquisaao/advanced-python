da700cfedc6bec2c24d66ecc388f1f4968593223
maquisaao <maxwpinheiro@gmail.com>
2025-09-20 19:47:57 -0300

    while dentro de while

--- while.py
+++ while.py
@@ -44,5 +44,20 @@ while contador <= 100:
     if contador == 40:
         break
 
+print('Acabou')
 
+'''
+while dentro de while
+'''
+
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+    linha += 1
 print('Acabou')
