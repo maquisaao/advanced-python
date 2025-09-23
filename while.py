3bdb0eeb861a1fe57a17183f807d5fd421e12de1
maquisaao <maxwpinheiro@gmail.com>
2025-09-22 23:19:24 -0300

    while else - recurso peculiar

--- while.py
+++ while.py
@@ -92,3 +92,18 @@ while indice < len(nome):
 novo_nome += '*'
 print(novo_nome)
 
+""" while/else """
+string = 'Valor qualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+
+    if letra == ' ':
+        break
+
+    print(letra)
+    i += 1
+else:
+    print('Não encontrei um espaço na string.')
+print('Fora do while.')
