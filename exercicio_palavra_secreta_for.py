92584f361d18c46d12114a2704af716f6bad6a87
maquisaao <maxwpinheiro@gmail.com>
2025-10-01 18:24:18 -0300

    desafio concluido

--- exercicio_palavra_secreta_for.py
+++ exercicio_palavra_secreta_for.py
@@ -15,9 +15,9 @@ Faça a contagem de tentativas do seu
 usuário.
 """
 
-
-# validar se a letra digitada está na palavra secreta 
-# guardar cada letra acertada (talvez concatenando com a lista das letras acertadas)
+palavra_secreta = 'jesus'
+letras_acertadas = ''
+qnt_letras_digitadas = 0
 
 while True:
     letra_digitada = input('Digite UMA letra:')
@@ -27,4 +27,20 @@ while True:
         print('Digite apenas UMA letra!')
         continue
 
-    
+    if letra_digitada in palavra_secreta: # validar se a letra digitada está na palavra secreta 
+        letras_acertadas += letra_digitada # guardar cada letra acertada e concatenar com as letras acertadas)
+        
+    palavra_digitada = '' # iterar a palavra secreta e se for correta, printar a letra, se nao for printar *
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_digitada += letra_secreta
+        else:
+            palavra_digitada += '*'
+    qnt_letras_digitadas += 1 # implementar as tentativas até finalizar o jogo
+
+    print(f'{palavra_digitada}')
+
+    if palavra_digitada == palavra_secreta: # sinalizar que venceu e quantas tentativas
+        print(f'Parabens, voce acertou a palavra secreta que era: {palavra_secreta}.')
+        print(f'Voce fez {qnt_letras_digitadas} tentativas.')
+        break
