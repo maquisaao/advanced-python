c3ffc65e60446ec5a2b1199f7f83f361418c6148
maquisaao <maxwpinheiro@gmail.com>
2025-10-03 16:56:46 -0300

    aula 2 - listas

--- listas.py
+++ listas.py
@@ -1,4 +1,5 @@
 """
+AULA1
 Listas em Python
 Tipo list - Mutável
 Suporta vários valores de qualquer tipo
@@ -17,3 +18,26 @@ lista = [123, True, 'Max Pinheiro',  1.2, []]
 lista[-3] = 'Ana Paula'
 print(lista)
 print(lista[2], type(lista[2]))
+
+"""
+AULA 2
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+#        0   1   2   3   4   5
+lista = [10, 20, 30, 40]
+# lista[2] = 300
+# del lista[2]
+# print(lista)
+# print(lista[2])
+lista.append(50)
+lista.pop()
+lista.append(60)
+lista.append(70)
+ultimo_valor = lista.pop(3)
+print(lista, 'Removido,', ultimo_valor)
