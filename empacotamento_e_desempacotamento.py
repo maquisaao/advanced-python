4c2aea702a78e30eb9e25713843a2bf16a3e3782
maquisaao <maxwpinheiro@gmail.com>
2025-10-22 10:42:49 -0300

    desempacotamento em chamadas de funcoes

--- empacotamento_e_desempacotamento.py
+++ empacotamento_e_desempacotamento.py
@@ -1,5 +1,30 @@
 """
 Introdução ao empacotamento e desempacotamento
 """
-_, _, nome, *resto = ['Maria', 'Helena', 'Luiz']
+_, _, nome, *resto = ['Maria', 'Helena', 'Max']
 print(nome)
+
+""" Desempacotamento em chamadas
+de métodos e funções """
+
+string = 'ABCD'
+lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
+tupla = 'Python', 'é', 'legal'
+salas = [
+    # 0        1
+    ['Maria', 'Helena', ],  # 0
+    # 0
+    ['Elaine', ],  # 1
+    # 0       1       2
+    ['Max', 'João', 'Eduarda', ],  # 2
+]
+
+# p, b, *_, ap, u = lista
+# print(p, u, ap)
+
+# print('Maria', 'Helena', 1, 2, 3, 'Eduarda')
+# print(*lista)
+# print(*string)
+# print(*tupla)
+
+print(*salas, sep='\n')
