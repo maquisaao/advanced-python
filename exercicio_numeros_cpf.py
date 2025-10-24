35789a2b7f0352411555874740f282d23b7d60a5
maquisaao <maxwpinheiro@gmail.com>
2025-10-24 10:59:56 -0300

    minha solução do exercicio

--- exercicio_numeros_cpf.py
+++ exercicio_numeros_cpf.py
@@ -35,7 +35,7 @@ for digito_1 in nove_digitos:
     contador_regressivo_1 -= 1 
 digito_1 = (resultado_digito_1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
-print(digito_1)
+print(f'O primeiro digito é: {digito_1}')
 
 """
 Calculo do segundo dígito do CPF
@@ -44,23 +44,20 @@ Colete a soma dos 9 primeiros dígitos do CPF,
 MAIS O PRIMEIRO DIGITO,
 multiplicando cada um dos valores por uma
 contagem regressiva começando de 11
-
-Ex.:  746.824.890-70 (7468248907)
-   11 10  9  8  7  6  5  4  3  2
-*  7   4  6  8  2  4  8  9  0  7 <-- PRIMEIRO DIGITO
-   77 40 54 64 14 24 40 36  0 14
-
-Somar todos os resultados:
-77+40+54+64+14+24+40+36+0+14 = 363
-Multiplicar o resultado anterior por 10
-363 * 10 = 3630
-Obter o resto da divisão da conta anterior por 11
-3630 % 11 = 0
-Se o resultado anterior for maior que 9:
-    resultado é 0
-contrário disso:
-    resultado é o valor da conta
-
-O segundo dígito do CPF é 0
 """
 
+cpf = '74682489070' # pode receber um cpf por input ou por banco de dados no futuro
+dez_digitos = [cpf[:9] + str(digito_1)] # juntar os 9 digitos com o digito 1 da primeira validação
+contador_regressivo_2 = 11
+
+resultado_digito_2 = 0
+# aplicaçao do algoritmo da receita pra validar digitos de cpf
+for digito_2 in dez_digitos:
+    resultado_digito_2 += int(digito_2) * contador_regressivo_2
+    contador_regressivo_2 -= 1 
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+print(f'O segundo digito é: {digito_2}')
+
+if str(digito_1) == cpf[9] and str(digito_2) == cpf[10]:
+    print(f'O cpf {cpf} está de acordo com o algoritmo da receita e tem seus digitos finais "{digito_1}" e "{digito_2}".')
